e137fdcce608849a7e260e045f3634cc6084a360
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-04-29 23:59:30 +0530

    add initial commit with models and basic expo and backend setups

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 8000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://ayandippaul284:maSkASgRS59Zrxx8@cluster0.v2hflza.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((error) => {
+    console.log("mongodb Connection failed", error);
+  });
+
+app.listen(PORT, () => {
+  console.log("server listning to port", PORT);
+});
